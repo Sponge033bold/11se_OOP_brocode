b4a2ab6dfd9a42698f9defda427fa9bfd5e878c7
Shahil <shahilsami09@gmail.com>
2026-05-01 14:38:28 +1000

    final completed

--- ParentDD.py
+++ ParentDD.py
@@ -7,21 +7,20 @@ class ParentDD:
         self.age = age
 
     def speak(self):
-        print(f"Hello my name is {self.name}, I am {self.age} years old, {self.catchphrase}, {self.name} loves relaxing at home.")
+        print(f"Hello my name is {self.name}, I am {self.age} years old, {self.catchphrase}, I love relaxing at home.")
 
 class ChildTL(ParentDD):
-
     def __init__(self, name, food, hobby, catchphrase, age):
         super().__init__(name, food, hobby, catchphrase, age)
 
-        self.activity = {
+        self.activities = {
             "Bart": "riding my skateboard",
             "Lisa": "reading my books",
             "Maggie": "playing with my toys"
         }
 
     def activity(self):
-        return self.activity.get(self.name, "Fortunately not in the Simpson family")
+        return self.activities.get(self.name, "fortunately not in the Simpson family")
     
     def speak(self):
-        print(f"Hello my name is {self.name}, I am {self.age} years old, {self.catchphrase}, I loves {self.activity()}.")
+        print(f"Hi my name is {self.name}, I am {self.age} years old, {self.catchphrase}, I loves {self.activity()}.")
--- mainDD.py
+++ mainDD.py
@@ -1,17 +1,13 @@
 from parentDD import ParentDD, ChildTL
 
-homer = ParentDD("Homer", "Donuts", "Watching TV", "D'oh!", 39)
-marge = ParentDD("Marge", "Pasta", "Gardening", "Hmm… I don't know, Homie.", 36)
-bart = ChildTL("Bart", "Pizza", "Skateboarding", "Eat my shorts!", 10)
-lisa = ChildTL("Lisa", "Salad", "Playing the saxophone", "If anyone wants me, I'll be in my room.", 8)
-maggie = ChildTL("Maggie", "Baby food", "Playing with her toys", "Goo goo ga ga!", 1)
+homer = ParentDD("Homer", "donuts", "watching TV", "D'oh!", 39)
+marge = ParentDD("Marge", "pasta", "gardening", "hmm... I don't know, Homie", 36)
+bart = ChildTL("Bart", "pizza", "skateboarding", "eat my shorts!", 10)
+lisa = ChildTL("Lisa", "salad", "playing the saxophone", "If anyone wants me, I'll be in my room", 8)
+maggie = ChildTL("Maggie", "baby food", "playing with her toys", "*suck* *suck*", 1)
 
 homer.speak()
 marge.speak()
 bart.speak()
 lisa.speak()
 maggie.speak()
-
-
-
-
